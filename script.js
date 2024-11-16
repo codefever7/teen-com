facbf85c41ae810eba2a455aafc9c750668729ad
kuldeep <codefever7@gmail.com>
2024-11-17 01:13:38 +0530

    seo freiendly

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let items = document.querySelectorAll(".faq-main .faq-item .faq-label");
-items.forEach(function (t) {
-  t.addEventListener("click", function (e) {
-    items.forEach(function (e) {
-      e !== t || e.classList.contains("faq-item-show")
-        ? e.classList.remove("faq-item-show")
-        : e.classList.add("faq-item-show");
+
+items.forEach(function (label) {
+  label.addEventListener("click", function () {
+    let parent = label.parentElement;
+    parent.classList.toggle("faq-item-show");
+    items.forEach(function (otherLabel) {
+      if (otherLabel !== label) {
+        otherLabel.parentElement.classList.remove("faq-item-show");
+      }
     });
   });
 });
